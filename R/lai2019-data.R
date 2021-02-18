6c90a56d65267d048db6c59371e5905cbfb8e26c
dataMaster-Kris <kchoudhary@ucdavis.edu>
2021-02-18 00:02:33 -0800

    Added data documentation, vignette in progress

---
+++ R/lai2019-data.R
@@ -0,0 +1,22 @@
+#' \emph{Saccharomyces cerevisiae} Structure-seq data
+#'
+#' Data from a Structure-seq assay of five samples of \emph{S. cerevisiae}, three
+#' of which were wild-type samples and two \emph{dbp2}\Delta samples. The data was
+#' pre-processed to obtain DMS reactivities as described by Lai et al. (2019).
+#'
+#' @docType data
+#'
+#' @usage data(lai2019)
+#'
+#' @format An object of class \code{"list"}.
+#'
+#' @keywords datasets
+#'
+#' @references Lai et al. (2019) Genetics, Vol. 212, 153–174
+#' (\href{https://doi.org/10.1534/genetics.119.302058}{Genetics})
+#'
+#' @source Raw data from \href{https://doi.org/10.1534/genetics.119.302058}{Lai et al., 2019} in processed form.
+#'
+#' @examples
+#' data(lai2019)
+"lai2019"
