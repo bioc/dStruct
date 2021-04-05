43278b584f9fd2b9f7a4d9e80f6c99c912c313d5
dataMaster-Kris <kchoudhary@ucdavis.edu>
2021-04-04 19:32:28 -0700

    Updated object name for function component operating on vector input to the function. The component operating on matrix input is fine.

--- R/calcDis.R
+++ R/calcDis.R
@@ -3,6 +3,6 @@
 #' @return If input is a numeric vector, a number is returned. For a matrix, a numeric vector is returned.
 #' @export
 calcDis <- function(x) {
-  if (class(x)== "numeric") return(2*(atan(abs(sd(y)/mean(y))))/pi )
+  if (class(x)== "numeric") return(2*(atan(abs(sd(x)/mean(x))))/pi )
   return(apply(x, 1, function(y) (2*(atan(abs(sd(y)/mean(y))))/pi ) ))
 }
